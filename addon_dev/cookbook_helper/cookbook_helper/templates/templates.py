036065342f9943156bd048402ca1c422221d84f6
Jake <jake@cow.cool>
2025-10-01 13:57:13 +0100

    v1.0.14

--- addon_dev/cookbook_helper/cookbook_helper/templates/templates.py
+++ addon_dev/cookbook_helper/cookbook_helper/templates/templates.py
@@ -1,47 +1,52 @@
-advancement = "{\n\
-  \"parent\": \"cnk:cookbook/root\",\n\
-  \"criteria\": {\n\
-    \"requirement\": {\n\
-      \"trigger\": \"minecraft:inventory_changed\",\n\
-      \"conditions\": {\n\
-        \"items\": [\n\
-          {\n\
-            \"items\": \"minecraft:poisonous_potato\",\n\
-            \"components\": {\n\
-              \"minecraft:custom_data\": {\"please_fill_this_in\":true}\n\
-            }\n\
-          }\n\
-        ]\n\
-      }\n\
-    }\n\
-  },\n\
-  \"rewards\": {\n\
-    \"function\": \"%(namespace)s:cookbook/grant/%(name)s\"\n\
-  }\n\
-}"
+advancement = """
+{
+  \"parent\": \"cnk:cookbook/root\",
+  \"criteria\": {
+    \"requirement\": {
+      \"trigger\": \"minecraft:inventory_changed\",
+      \"conditions\": {
+        \"items\": [
+          {
+            \"items\": \"minecraft:poisonous_potato\",
+            \"components\": {
+              \"minecraft:custom_data\": {\"please_fill_this_in\":true}
+            }
+          }
+        ]
+      }
+    }
+  },
+  \"rewards\": {
+    \"function\": \"%(namespace)s:cookbook/grant/%(name)s\"
+  }
+}
+"""
 
-toast = "{\n\
-  \"parent\": \"cnk:cookbook/toasts\",\n\
-  \"display\": {\n\
-    \"title\": [{\"translate\":\"book.cnk.toast.background\",\"font\":\"cnk.book:advancement\"},{\"translate\":\"book.cnk.toast.unlock.ingredient\",\"font\":\"cnk.book:advancement_text\",\"color\":\"#7b613a\"}],\n\
-    \"icon\": {\n\
-      \"id\": \"minecraft:poisonous_potato\",\n\
-      \"components\": {\"minecraft:item_model\": \"%(namespace)s:%(name)s\"}\n\
-    },\n\
-    \"description\": \"\",\n\
-    \"announce_to_chat\": false\n\
-  },\n\
-  \"criteria\": {\n\
-    \"requirement\": {\n\
-      \"trigger\": \"minecraft:impossible\"\n\
-    }\n\
-  }\n\
-}"
+toast = """
+{
+  \"parent\": \"cnk:cookbook/toasts\",
+  \"display\": {
+    \"title\": [{\"translate\":\"book.cnk.toast.background\",\"font\":\"cnk.book:advancement\"},{\"translate\":\"book.cnk.toast.unlock.ingredient\",\"font\":\"cnk.book:advancement_text\",\"color\":\"#7b613a\"}],
+    \"icon\": {
+      \"id\": \"minecraft:poisonous_potato\",
+      \"components\": {\"minecraft:item_model\": \"%(namespace)s:%(name)s\"}
+    },
+    \"description\": \"\",
+    \"announce_to_chat\": false
+  },
+  \"criteria\": {
+    \"requirement\": {
+      \"trigger\": \"minecraft:impossible\"
+    }
+  }
+}
+"""
 
-grant_flag = "\
-advancement grant @s[tag=!cnk.cookbook_unlock,tag=!cnk.no_toasts] only %(namespace)s:cookbook/%(name)s/toast\n\
-\n\
-function cnk:cookbook/database/set/main {flag:\"%(type)s.%(namespace)s.%(name)s\"}"
+grant_flag = """
+advancement grant @s[tag=!cnk.cookbook_unlock,tag=!cnk.no_toasts] only %(namespace)s:cookbook/%(name)s/toast
+
+function cnk:cookbook/database/set/main {flag:\"%(type)s.%(namespace)s.%(name)s\"}
+"""
 
 font = "{\n    \"providers\": [\n%(characters)s    ]\n}"
 
--- data/cnk/function/mixing_bowl/hit/check.mcfunction
+++ data/cnk/function/mixing_bowl/hit/check.mcfunction
@@ -2,7 +2,7 @@ execute if score $hit_mixing_bowl_check cnk.dummy matches 1 run return fail
 
 execute on attacker if entity @s[tag=cnk.hit_mixing_bowl] run scoreboard players set $hit_mixing_bowl_check cnk.dummy 1
 execute if score $hit_mixing_bowl_check cnk.dummy matches 1 run data remove entity @s attack
-execute if score $hit_mixing_bowl_check cnk.dummy matches 1 on vehicle on passengers if entity @s[type=minecraft:item_display,tag=cnk.mixing_bowl_item] on vehicle run return run function cnk:mixing_bowl/hit/remove_last
+execute if score $hit_mixing_bowl_check cnk.dummy matches 1 on vehicle unless entity @s[tag=cnk.mixing] on passengers if entity @s[type=minecraft:item_display,tag=cnk.mixing_bowl_item] on vehicle run return run function cnk:mixing_bowl/hit/remove_last
 execute if score $hit_mixing_bowl_check cnk.dummy matches 1 on vehicle run scoreboard players add @s cnk.hit_count 1
 execute if score $hit_mixing_bowl_check cnk.dummy matches 1 on vehicle at @s run playsound minecraft:block.bamboo_wood.hit block @a ~ ~ ~ 1 2
 execute if score $hit_mixing_bowl_check cnk.dummy matches 1 on vehicle at @s run particle minecraft:item{item:{id:"minecraft:spruce_planks"}} ~ ~-0.25 ~ 0.3 0.1 0.3 0.1 5
